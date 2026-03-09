c89735fcb9a7a3c9d9ca7a63130ad0d9cd57b5bd
joão victor <joaorob.m.l@gmail.com>
2026-03-09 16:11:50 -0300

    Fix soma2 test expectation and update coverage

--- test/soma.test.js
+++ test/soma.test.js
@@ -1,5 +1,11 @@
 const soma = require('../soma');
+const soma2 = require('../soma2');
 
 test('soma 1 + 2 e igual a 3', () => {
     expect(soma(1, 2)).toBe(3);
 });
+
+test('soma2 1 + 2 e igual a 3', () => {
+    expect(soma2(1, 2)).toBe(3);
+});
+
